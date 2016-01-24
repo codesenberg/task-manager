e8861cbd06aa8f42864a114a164f8dd3c8297666
Maksim Fedoseyev <max.faceless.frei@gmail.com>
2016-01-24 18:04:38 +0500

    Changed stored attachments directory.

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -1,6 +1,6 @@
 class AttachmentUploader < CarrierWave::Uploader::Base
   def store_dir
-    Rails.root.join("private/uploads/#{model.class.name.underscore}/#{model.id}")
+    Rails.root.join("private/#{Rails.env}/uploads/#{model.class.name.underscore}/#{mounted_as}/#{model.id}")
   end
 
   def cache_dir
